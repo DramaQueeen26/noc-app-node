4de441e9cd47b1b060043ae0ba605ce32feef58f
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-15 15:22:26 -0400

    Schema & Models Mongo

---
+++ src/data/mongo/models/log.model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const logSchema = new Schema({
+
+  level: {
+    enum: ['low', 'medium', 'high'],
+    default: 'low'
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  origin: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+
+})
+
+export const LogModel = model('Log', logSchema )
