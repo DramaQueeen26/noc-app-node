df23b5659966275d4f92952053f5e2e27ea52ed8
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-18 13:48:28 -0400

    Pruebas en la conexión de MongoDB

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -16,6 +16,8 @@ export class MongoDatabase {
       await mongoose.connect( mongoUrl, { dbName: dbName })
       console.log('Mongo connected');
 
+      return true
+
     } catch (error) {
       
       console.log('Mongo connection error');
