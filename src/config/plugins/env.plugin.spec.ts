eed47c448204495a142f6c2a22628304b98487e4
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-09-18 13:38:46 -0400

    Pruebas en ENVs

---
+++ src/config/plugins/env.plugin.spec.ts
@@ -0,0 +1,44 @@
+import { envs } from './env.plugin';
+
+
+
+describe('envs.plugin.ts', () => {
+
+
+
+  test('should return env options', ()=> {
+
+    expect( envs ).toEqual({
+      PROD: false,
+      URL: 'https://google.com/',
+      MONGO_URL: 'mongodb+srv:',
+      MONGO_DB_NAME: 'NOC-TEST',
+      MAILER_SERVICE: 'gmail',
+      MAILER_EMAIL: 'test@gmail.com',
+      MAILER_SECRET_KEY: 'asdad'
+    });
+
+
+  });
+
+
+  test('should return error if not found env', async() => {
+    
+    jest.resetModules();
+    process.env.PROD = 'truee';
+
+    try {
+      await import('./env.plugin');
+      expect(true).toBe(false);
+
+
+    } catch (error) {
+      expect(`${error}`).toContain('"PROD" should be either "true", "false", "TRUE", "FALSE", 1, or 0');
+    }
+    
+
+  })
+
+
+
+})
